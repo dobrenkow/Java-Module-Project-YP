4c4ff3f58c4eb8332e7bab8ff132f8901c91ebde
dobrenkow <dobrenkow2012@yandex.ru>
2023-06-02 21:31:35 +0300

    Подходим к завершению.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    static int humans;
-    Products PC = new Products();
     static double sumMain = 0;
     static String strProductsMain = "";
+    private static int humans;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("На сколько человек необходимо разделить счет?");
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println ("Введите целое число");
-            humans = scanner.nextInt();
-                if (humans > 1) {
-                    break;
-                } else if (humans == 1) {
-                    System.out.println("Нет смысла ничего считать и делить :-)");
-                    return;
-                } else {
-                    System.out.println("Некорректное значение для подсчёта!");
-                }break;
+            System.out.println("Введите целое число");
+            try {
+                humans = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Не целое число");
+                scanner.nextLine();
             }
-               Products.productCount();
+            if (humans > 1) {
+                break;
+            } else if (humans == 1) {
+                System.out.println("Нет смысла ничего считать и делить :-)");
+                return;
+            } else {
+                System.out.println("Некорректное значение для подсчёта!");
+            }
+        }
+        Products.productCount();
         strProductsMain += Products.strProducts;
         System.out.println("Добавленные товары: " + strProductsMain);
         sumMain += Products.sum;
@@ -28,35 +34,37 @@ public class Main {
         String result = String.format("%.2f", sumMain);
         int floor = (int) Math.floor(sumMain);
         String a = null;
-        if (floor%100==11) {
+        if (floor % 100 == 11) {
             a = "рублей";
-        }else if (floor%100==12){
+        } else if (floor % 100 == 12) {
             a = "рублей";
-        }else if (floor%100==13) {
+        } else if (floor % 100 == 13) {
             a = "рублей";
-        }else if (floor%100==14){
+        } else if (floor % 100 == 14) {
             a = "рублей";
-        }else if (floor%10==1) {
+        } else if (floor % 10 == 1) {
             a = "рубль";
-        }else if (floor%10==2){
+        } else if (floor % 10 == 2) {
             a = "рубля";
-        }else if (floor%10==3) {
+        } else if (floor % 10 == 3) {
             a = "рубля";
-        }else if (floor%10==4) {
+        } else if (floor % 10 == 4) {
             a = "рубля";//
-        }else if (floor%10==5) {
+        } else if (floor % 10 == 5) {
             a = "рублей";
-        }else if (floor%10==6){
+        } else if (floor % 10 == 6) {
             a = "рублей";
-        }else if (floor%10==7) {
+        } else if (floor % 10 == 7) {
             a = "рублей";
-        }else if (floor%10==8){
+        } else if (floor % 10 == 8) {
             a = "рублей";
-        }else if (floor%10==9) {
+        } else if (floor % 10 == 9) {
             a = "рублей";
-        }else if (floor%10==0) {
-            a = "рублей";}
-        System.out.println("Всем необходиомо заплатить по " + result +" " + a +".");
+        } else if (floor % 10 == 0) {
+            a = "рублей";
+        }
+        System.out.println("Всем необходиомо заплатить по " + result + " " + a + ".");
 
 
-}}
+    }
+}
--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -1,54 +1,64 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Products {
     static String strProducts = "";
     static double sum = 0;
-     static void productCount() {
-         Scanner scanner = new Scanner(System.in);
-         while (true) {
-             System.out.println("Хотите добавить товар?");
-             String addProduct = scanner.next();
-             switch (addProduct.toLowerCase()) {
-                 case "да":
-                     while (true) {
-                         System.out.println("Введите название товара.");
-                         String productName = scanner.next();
-                         strProducts = strProducts.concat("\n").concat(productName);
-                         double productPrice;
-                         while (true) {
-                             System.out.println("Введите стоимость товара.");
-                             productPrice = scanner.nextDouble();
-                             if (productPrice >= 0) {
-                                 break;
-                             }else if (productPrice < 0){
-                                 System.out.println("Стоимость не может быть отрицательной!");
-                         }}
-                         System.out.println("Товар добавлен!");
-                         sum = sum + productPrice;
-                         System.out.println("Хотите добавить еще товар? (Введите \"Завершить\" ,чтобы завершить процесс добавления товаров.)");
-                         String addProduct2 = scanner.next();
-                         switch (addProduct2.toLowerCase()) {
-                             case "да":
-                                 break;
-                             case "нет":
-                                 return;
-                             case "завершить":
-                                 return;
-                             default:
-                                 while (true) {
-                                     System.out.println("Введите да или Завершить!");
-                                     String addProduct3 = scanner.next();
-                                     if (addProduct3.equalsIgnoreCase("да")) {
-                                         break;
-                                     } else if (addProduct3.equalsIgnoreCase("Завершить")) {
-                                         return;
-                                     } else {
-                                     }
-                                 }
-                         }
-                     }
-                 case "нет":
-                     return;
-                 default:
-                     System.out.println("Введите да или нет!");
-     }}}}
+    static double productPrice;
+    static void productCount() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Хотите добавить товар?");
+            String addProduct = scanner.next();
+            switch (addProduct.toLowerCase()) {
+                case "да":
+                    while (true) {
+                        System.out.println("Введите название товара.");
+                        String productName = scanner.next();
+                        strProducts = strProducts.concat("\n").concat(productName);
+                        while (true) {
+                            System.out.println("Введите стоимость товара.");
+                            try {
+                                productPrice = scanner.nextDouble();
+                            }catch (InputMismatchException b){
+                                System.out.println("Не целое число");
+                                scanner.nextLine();
+                            }
+                            if (productPrice > 0) {
+                                break;
+                            } else if (productPrice < 0) {
+                                System.out.println("Стоимость не может быть отрицательной!");
+                            }
+                        }
+                        System.out.println("Товар добавлен!");
+                        sum = sum + productPrice;
+                        System.out.println("Хотите добавить еще товар? (Введите \"Завершить\" ,чтобы завершить процесс добавления товаров.)");
+                        String addProduct2 = scanner.next();
+                        switch (addProduct2.toLowerCase()) {
+                            case "да":
+                                break;
+                            case "нет":
+                                return;
+                            case "завершить":
+                                return;
+                            default:
+                                while (true) {
+                                    System.out.println("Введите да или Завершить!");
+                                    String addProduct3 = scanner.next();
+                                    if (addProduct3.equalsIgnoreCase("да")) {
+                                        break;
+                                    } else if (addProduct3.equalsIgnoreCase("Завершить")) {
+                                        return;
+                                    } else {
+                                    }
+                                }
+                        }
+                    }
+                case "нет":
+                    return;
+                default:
+                    System.out.println("Введите да или нет!");
+            }
+        }
+    }
+}
